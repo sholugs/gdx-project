31aed8be3a0f7535f2b83ec626365445b42bea03
Gabriel Cavasso <gaerca123@gmail.com>
2023-05-01 15:38:17 -0300

    feat CRUD user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { USER_ROLE } from 'src/constants/enum/role';
+import { RegisterUserDto } from './dto/register-user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -9,4 +11,25 @@ export class UserController {
   public async allUsers() {
     return this.userService.findAll();
   }
+
+  @Get(':id')
+  public async oneUser() {
+    return this.userService.findOne('id');
+  }
+
+  @Get('username/:username')
+  public async oneUserByUsername() {
+    return this.userService.findOneByUsername('username');
+  }
+
+  @Post()
+  public async createUser(@Body() user: RegisterUserDto) {
+    return this.userService.create(user);
+  }
+  
+  @Delete(':id')
+  public async deleteUser(@Param('id') id: string) {
+    return this.userService.remove('id');
+  }
+
 }
